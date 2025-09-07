68bbdcdbe43f0ed76ae70d8b1c20f5954824935b
Kapil <kapilraut026@gmail.com>
2025-09-07 09:49:19 +0545

    hi

--- ai/app/dashboard/layout.tsx
+++ ai/app/dashboard/layout.tsx
@@ -1,13 +1,13 @@
 import { ClerkLoaded } from "@clerk/nextjs";
 import React from "react";
-import Header from "@/components/ui/ui/header";
+import Header from "@/components/Header";
 
 const layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <ClerkLoaded>
       <div className="flex-1 flex flex-col h-screen ">
         <Header />
-        {children}
+        <main className="flex-1 overflow-y-auto">{children}</main>
       </div>
     </ClerkLoaded>
   );
--- ai/app/dashboard/page.tsx
+++ ai/app/dashboard/page.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import Documents from "@/components/Documents";
 
-function page() {
+export const dynamic = "force-dynamic";
+
+function Dashboard() {
   return (
-    <div>page</div>
-  )
-}
+    <div className="h-full max-w-7xl mx-auto">
+      <h1 className="text-3xl p-5 bg-gray-100 font-extralight text-indigo-600">
+        My Documents
+      </h1>
 
-export default page
+      <Documents />
+    </div>
+  );
+}
+export default Dashboard;
